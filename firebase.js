2c3b8c14174d93edeaf311e2d47f71227d2d17a5
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-05-07 14:21:31 +0300

    move variables to env

--- firebase.js
+++ firebase.js
@@ -1,9 +1,20 @@
 const firebaseAdmin = require("firebase-admin");
 
-var serviceAccount = require("./service-account.json");
-
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
+  credential: firebaseAdmin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_x509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_x509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN,
+  }),
 });
 
 const db = firebaseAdmin.firestore();
